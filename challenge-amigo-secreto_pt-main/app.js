eba135df718de5dd833c91d8295a3419dabb9020
Celina-colares <celinacolares3@gmail.com>
2025-02-06 10:52:14 -0400

    Projeto finalizado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,3 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
 
-// testando comando teste 
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    amigos.push(nome);
+
+    input.value = '';
+
+    atualizarLista();
+}
+
+function atualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione pelo menos 1 amigo para sortear.');
+        return;
+    }
+
+    let amigoIndex = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[amigoIndex];
+    let listaResultado = document.getElementById('resultado');
+    listaResultado.innerHTML = '';
+
+    const li = document.createElement('li');
+    li.textContent = `Amigo sorteado: ${amigoSorteado}`;
+    listaResultado.appendChild(li);
+}
